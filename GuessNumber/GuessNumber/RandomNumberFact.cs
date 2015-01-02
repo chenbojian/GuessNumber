caf76e3008d64589ad0a0f784e4f9260904edfb0
chenbojian <chenbojian@foxmail.com>
2015-01-03 01:53:30 +0800

    RandomNumber complete

--- GuessNumber/GuessNumber/RandomNumberFact.cs
+++ GuessNumber/GuessNumber/RandomNumberFact.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System.Linq;
+using Xunit;
 
 namespace GuessNumber
 {
@@ -8,22 +9,53 @@ namespace GuessNumber
         [Fact]
         public void should_return_1234_when_seed_return_1234()
         {
-            IRandomSeed seed = new TestRandomSeed("1234");
-            var generator = new RandomNumber(seed);
+            IRandomSeed seed = new TestRandomSeed(new[] {"1234"});
+            var generator = new RandomNumberGenerator(seed);
             var randomNumber = generator.Generate();
             Assert.Equal("1234", randomNumber);
         }
-    }
 
-    public interface IRandomSeed
-    {
+        [Fact]
+        public void should_return_1234_when_seed_return_2334_1234()
+        {
+            IRandomSeed seed = new TestRandomSeed(new[] {"2334", "1234"});
+            var generator = new RandomNumberGenerator(seed);
+            var randomNumber = generator.Generate();
+            Assert.Equal("1234", randomNumber);
+        }
+
+        [Fact]
+        public void should_return_5634_when_seed_return_2334_1334_5634()
+        {
+            IRandomSeed seed = new TestRandomSeed(new[] {"2334", "1334", "5634"});
+            var generator = new RandomNumberGenerator(seed);
+            var randomNumber = generator.Generate();
+            Assert.Equal("5634", randomNumber);
+        }
+
+        [Fact]
+        public void should_return_number_when_call_generator()
+        {
+            var generator = new RandomNumberGenerator(new RandomSeed());
+            var randomNumber = generator.Generate();
+            Assert.Equal(4, randomNumber.Distinct().Count());
+        }
     }
 
     public class TestRandomSeed : IRandomSeed
     {
-        public TestRandomSeed(string s)
+        private readonly string[] _numberList;
+        private int _count;
+
+        public TestRandomSeed(string[] numberList)
         {
-            throw new System.NotImplementedException();
+            _numberList = numberList;
+            _count = 0;
+        }
+
+        public string GetRandomNumber()
+        {
+            return _numberList[_count++];
         }
     }
 }
---
+++ GuessNumber/GuessNumber/RandomNumberGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace GuessNumber
+{
+    public interface IRandomSeed
+    {
+        string GetRandomNumber();
+    }
+
+    class RandomSeed : IRandomSeed
+    {
+        public string GetRandomNumber()
+        {
+            Random random = new Random();
+            int number = random.Next(100, 9999);
+            return (number < 1000) ? "0" + number : number.ToString();
+        }
+    }
+
+    public class RandomNumberGenerator
+    {
+        private readonly IRandomSeed _seed;
+
+        public RandomNumberGenerator(IRandomSeed seed)
+        {
+            _seed = seed;
+        }
+
+        public string Generate()
+        {
+            string randomNumber = _seed.GetRandomNumber();
+            if (randomNumber.Distinct().Count() != randomNumber.Count())
+            {
+                return Generate();
+            }
+            return randomNumber;
+        }
+    }
+}
