62d404c63ca136c8c5f34e1c912c2ba2342cd3a0
chenbojian <chenbojian@foxmail.com>
2015-01-02 23:42:48 +0800

    passed all A's tests

--- GuessNumber/GuessNumber/GuessNumber.cs
+++ GuessNumber/GuessNumber/GuessNumber.cs
@@ -2,9 +2,17 @@
 {
     public class GuessNumber
     {
-        public static string Compare(int inputNumber, int userGuess)
+        public static string Compare(string inputNumber, string userGuess)
         {
-            
+            int countA = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                if (inputNumber[i] == userGuess[i])
+                {
+                    countA++;
+                }
+            }
+            return countA.ToString() + "A0B";
         }
     }
 }
--- GuessNumber/GuessNumber/GuessNumberFact.cs
+++ GuessNumber/GuessNumber/GuessNumberFact.cs
@@ -6,13 +6,24 @@ namespace GuessNumber
         [Fact]
         public void should_return_0A0B_when_compare_1234_and_5678()
         {
-            Assert.Equal("0A0B", GuessNumber.Compare(1234,5678));
+            Assert.Equal("0A0B", GuessNumber.Compare("1234", "5678"));
         }
 
         [Fact]
         public void should_return_1A0B_when_compare_1234_and_1678()
         {
-            Assert.Equal("1A0B", GuessNumber.Compare(1234, 1678));
+            Assert.Equal("1A0B", GuessNumber.Compare("1234", "1678"));
         }
+        [Fact]
+        public void should_return_2A0B_when_compare_1234_and_1638()
+        {
+            Assert.Equal("2A0B", GuessNumber.Compare("1234", "1638"));
+        }
+        [Fact]
+        public void should_return_4A0B_when_compare_1234_and_1234()
+        {
+            Assert.Equal("4A0B", GuessNumber.Compare("1234", "1234"));
+        }
+
     }
 }
