81c74ec8f77ed7123d44f7ee2065a0d7059a01a2
chenbojian <chenbojian@foxmail.com>
2015-01-03 00:18:42 +0800

    add RandomNumber test

---
+++ GuessNumber/GuessNumber/RandomNumber.cs
@@ -0,0 +1,15 @@
+﻿namespace GuessNumber
+{
+    public class RandomNumber
+    {
+        public RandomNumber(IRandomSeed seed)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string Generate()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ GuessNumber/GuessNumber/RandomNumberFact.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace GuessNumber
+{
+    public class RandomNumberFact
+    {
+        //use mock to test random generator
+        [Fact]
+        public void should_return_1234_when_seed_return_1234()
+        {
+            IRandomSeed seed = new TestRandomSeed("1234");
+            var generator = new RandomNumber(seed);
+            var randomNumber = generator.Generate();
+            Assert.Equal("1234", randomNumber);
+        }
+    }
+
+    public interface IRandomSeed
+    {
+    }
+
+    public class TestRandomSeed : IRandomSeed
+    {
+        public TestRandomSeed(string s)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
